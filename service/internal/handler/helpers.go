394d03a01802737c1b0e8d6cdc46cae428a7daa2
Idris Akkulov <i.akkulov@gmail.com>
2024-01-02 19:35:49 +0300

    helpers

---
+++ service/internal/handler/helpers.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+func MethodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(405)
+	render.Render(w, r, ErrMethodNotAllowed)
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(400)
+	render.Render(w, r, ErrNotFound)
+}
